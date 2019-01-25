a390874995b45ee92fd9e73cdf85b27b4dac9651
cryptokitties0327 <kimberley_tse@yahoo.com.au>
2019-01-25 12:04:14 +1100

    rosterbackend

--- models/Roster.js
+++ models/Roster.js
@@ -1,20 +1,38 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const rosterSchema = new Schema({
+//   startDate: Date,
+//   endDate: Date,
+//   location: String,
+//   shifts:[
+//     {
+//     day: String,
+//     staff: 
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User',
+//         startTime: Date,
+//         endTime: Date
+//       }
+//     }
+//   ]
+// });
+
+// module.exports = Roster = mongoose.model('Roster', rosterSchema);
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const rosterSchema = new Schema({
-  startDate: Date,
-  endDate: Date,
+  date: Date,
   location: String,
-  shifts:[
+  staff: [
     {
-    day: String,
-    staff: 
-      {
-        type: mongoose.Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         startTime: Date,
         endTime: Date
-      }
     }
   ]
 });
