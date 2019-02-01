746db28bab0d8e8d122cd201bbf9d4254b01f20c
cryptokitties0327 <kimberley_tse@yahoo.com.au>
2019-02-01 11:13:44 +1100

    changes in authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,40 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const saltRounds = 10
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const saltRounds = 10;
 
-const User = require('../models/User');
-const Roster = require('../models/Roster');
+const User = require("../models/User");
+const Roster = require("../models/Roster");
 
 const authenticateUser = (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) { return next(err) }
-    if (!user) { return res.status(401).send(info.message) }
-    req.logIn(user, (err) => {
-      if (err) { return next(err) }
-      return res.send('Successfully authenticated');
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).send(info.message);
+    }
+    req.logIn(user, err => {
+      if (err) {
+        return next(err);
+      }
+      return res.send("Successfully authenticated");
     });
   })(req, res, next);
-}
+};
 
 const isAuthenticated = (req, res, next) => {
-  if(!req.user) {
-    return res.status(403).send('Not authorized!');
+  if (!req.user) {
+    return res.status(403).send("Not authorized!");
   }
   next();
-}
+};
 
 const isAdmin = (req, res, next) => {
-  if(!req.user.admin) {
-    return res.status(403).send('Not authorized!');
+  if (!req.user.admin) {
+    return res.status(403).send("Not authorized!");
   }
   next();
-}
+};
 
-router.post('/login', authenticateUser);
+router.post("/login", authenticateUser);
 
-router.post('/register', isAuthenticated, isAdmin, (req, res) => {
+router.post("/register", isAuthenticated, isAdmin, (req, res) => {
   const { firstName, lastName, mobile, email, password } = req.body;
   const salt = bcrypt.genSaltSync(saltRounds);
   const hash = bcrypt.hashSync(password, salt);
@@ -46,14 +52,15 @@ router.post('/register', isAuthenticated, isAdmin, (req, res) => {
     password: hash
   });
 
-  user.save()
+  user
+    .save()
     .then(doc => {
       res.send(`${doc.firstName} has been created`);
     })
-    .catch(err => res.status(401).send('bad request'))
+    .catch(err => res.status(401).send("bad request"));
 });
 
-router.post('/roster', isAuthenticated, isAdmin, (req, res) => {
+router.post("/roster", isAuthenticated, isAdmin, (req, res) => {
   const { date, location, staff } = req.body;
   const roster = new Roster({
     date,
@@ -61,112 +68,112 @@ router.post('/roster', isAuthenticated, isAdmin, (req, res) => {
     staff
   });
 
-  roster.save()
+  roster
+    .save()
     .then(doc => {
       res.send(`${doc.location} has been created`);
     })
-    .catch(err => res.status(401).send(err))
+    .catch(err => res.status(401).send(err));
 });
 
-router.get('/unavailibility', isAuthenticated, isAdmin, (req, res) => {
-  const allUnavail = []
+router.get("/unavailibility", isAuthenticated, isAdmin, (req, res) => {
+  const allUnavail = [];
 
-  User.find({})
-    .then(staff => {
-      staff.forEach(doc => {
-        const user = doc.firstName
-        const unavail = doc.unavailability
-        allUnavail.push({user, unavail})
-      })
-      res.send(allUnavail)
-    })
+  User.find({}).then(staff => {
+    staff.forEach(doc => {
+      const user = doc.firstName;
+      const unavail = doc.unavailability;
+      allUnavail.push({ user, unavail });
+    });
+    res.send({ allUnavailability: allUnavail });
+  });
+});
 
-})
+router.get("/unavailibility/:id", isAuthenticated, (req, res) => {
+  const id = req.params.id;
 
-router.get('/unavailibility/:id', isAuthenticated, (req, res) => {
-  const id = req.params.id
-  
-  User.findOne({
-    "_id": id
-  }, function(err, user) {
-    res.send(user.unavailability)
-  })
-  
-})
+  User.findOne(
+    {
+      _id: id
+    },
+    function(err, user) {
+      res.send(user.unavailability);
+    }
+  );
+});
 
-router.put('/unavailability/:id', isAuthenticated,  (req, res) => {
-  const _id = req.params.id
-  const { unavailability } = req.body
+router.put("/unavailability/:id", isAuthenticated, (req, res) => {
+  const _id = req.params.id;
+  const { unavailability } = req.body;
 
   User.findOneAndUpdate(
-      { _id },
-      { $push: {unavailability} },
-      {
-          new: true,
-          runValidators: true
-      }
-  )
-  .then(doc => res.send(doc));
-
-})
-
-router.delete('/unavailability/:id/:unid', isAuthenticated,  (req, res) => {
-  const id = req.params.id
-  const unid = req.params.unid
-  
-  User.findOne({
-    "_id": id
-  }, function(err, user) {
-    user.unavailability.forEach(un => {
-      if(un._id == unid) {
-        user.unavailability.remove(un)
-        user.save()
-      }
-    })
-    res.send(user)
-  })
-
-})
-
-router.get('/roster', isAuthenticated, (req,res)=>{
-  Roster.find({})
-        .then(doc => {
-          return res.send(doc)
-    })
+    { _id },
+    { $push: { unavailability } },
+    {
+      new: true,
+      runValidators: true
+    }
+  ).then(doc => res.send(doc));
 });
 
-router.get('/roster/:id/', isAuthenticated, (req,res)=>{
-    const { id } = req.params;
-    Roster.findOne({
-      "_id": id
-    }, function(err, roster) {
-      res.send(roster);
-    });
-});
+router.delete("/unavailability/:id/:unid", isAuthenticated, (req, res) => {
+  const id = req.params.id;
+  const unid = req.params.unid;
 
-
-router.put('/roster/:id', isAuthenticated, isAdmin, (req, res) => {
-    const _id = req.params.id
-    const { date, location, staff } = req.body
-
-    Roster.findOneAndUpdate(
-        { _id },
-        { date, location, staff },
-        {
-            new: true,
-            runValidators: true
+  User.findOne(
+    {
+      _id: id
+    },
+    function(err, user) {
+      user.unavailability.forEach(un => {
+        if (un._id == unid) {
+          user.unavailability.remove(un);
+          user.save();
         }
-    )
-    .then(doc => res.send(doc));
-
-})
-
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.send('Successfully logged out');
+      });
+      res.send(user);
+    }
+  );
 });
 
-router.get('/me', (req, res) => {
+router.get("/roster", isAuthenticated, (req, res) => {
+  Roster.find({}).then(doc => {
+    return res.send(doc);
+  });
+});
+
+router.get("/roster/:id/", isAuthenticated, (req, res) => {
+  const { id } = req.params;
+  Roster.findOne(
+    {
+      _id: id
+    },
+    function(err, roster) {
+      res.send(roster);
+    }
+  );
+});
+
+router.put("/roster/:id", isAuthenticated, isAdmin, (req, res) => {
+  const _id = req.params.id;
+  const { date, location, staff } = req.body;
+
+  Roster.findOneAndUpdate(
+    { _id },
+    { date, location, staff },
+    {
+      new: true,
+      runValidators: true
+    }
+  ).then(doc => res.send(doc));
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send("Successfully logged out");
+});
+
+router.get("/me", (req, res) => {
   res.send(req.user);
 });
 
